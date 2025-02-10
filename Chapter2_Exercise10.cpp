0b07e304aa11022a5859332b682008268b943cbb
BryanH <Bryan_c_hurley@outlook.com>
2025-02-09 22:05:41 -0500

    Add project files.

---
+++ Chapter2_Exercise10.cpp
@@ -0,0 +1,17 @@
+/* Program File Name: Chapter2_Exercise10.cpp
+Programmer: Bryan Hurley
+Date:Feburary 2025
+Requirements: A car holds 15 gallons of gasoline and can travel 375 miles before refueling. Write a program that
+calculates the number of miles per gallon the car gets. Display the result on the screen.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int capacity, miles, MPG;
+    capacity = 15;
+    miles = 375;
+    MPG = 375 / 15;
+    cout << "A car that holds " << capacity << " gallons of gasoline and can travel " << miles << " miles has a MPG of " << MPG << "." << endl;
+}
